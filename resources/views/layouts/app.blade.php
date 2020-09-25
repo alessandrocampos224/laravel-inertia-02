23d8029bd0b1acba58a3cbda7917fed7d58aeec4
Alessandro Campos <alessandrocampos224@gmail.com>
2020-09-25 19:24:41 -0300

    minhas alterações

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,24 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600" rel="stylesheet">
 
+    <!-- BEGIN: Vendor CSS-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('_dist/admin/vendors/css/vendors.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('_dist/admin/vendors/css/charts/apexcharts.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('_dist/admin/vendors/css/extensions/tether-theme-arrows.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('_dist/admin/vendors/css/extensions/tether.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('_dist/admin/vendors/css/extensions/shepherd-theme-default.css') }}">
+    <!-- END: Vendor CSS-->
+
+     <!-- BEGIN: Page CSS-->
+     <link rel="stylesheet" type="text/css" href="_dist/admin/css/core/menu/menu-types/vertical-menu.css">
+    <link rel="stylesheet" type="text/css" href="_dist/admin/css/core/colors/palette-gradient.css">
+    <link rel="stylesheet" type="text/css" href="_dist/admin/css/pages/dashboard-analytics.css">
+    <link rel="stylesheet" type="text/css" href="_dist/admin/css/pages/card-analytics.css">
+    <link rel="stylesheet" type="text/css" href="_dist/admin/css/plugins/tour/tour.css">
+    <!-- END: Page CSS-->
+     <!-- BEGIN: Custom CSS-->
+     <link rel="stylesheet" type="text/css" href="_dist/admin/css/style.css">
+    <!-- END: Custom CSS-->
     {{-- Theme Styles --}}
     <link rel="stylesheet" href="{{ asset(mix('_dist/admin/css/bootstrap.css')) }}">
     <link rel="stylesheet" href="{{ asset(mix('_dist/admin/css/bootstrap-extended.css')) }}">
@@ -32,9 +50,31 @@
     {{-- Page Styles --}}
     {{-- Laravel Style --}}
     <link rel="stylesheet" href="{{ asset(mix('_dist/admin/css/custom-rtl.css')) }}">
-    <link rel="stylesheet" href="{{ asset(mix('_dist/admin/css/pages/authentication/authentication.css')) }}">
+    <!-- <link rel="stylesheet" href="{{ asset(mix('_dist/admin/css/pages/authentication/authentication.css')) }}"> -->
 </head>
-<body class="horizontal-layout horizontal-menu 1-column  navbar-floating footer-static bg-full-screen-image  blank-page blank-page">
+<body class="vertical-layout vertical-menu-modern 2-columns  navbar-floating footer-static  " data-open="click" data-menu="vertical-menu-modern" data-col="2-columns">
     @call
+
+
+    <!-- BEGIN: Vendor JS-->
+    <script src="_dist/admin/vendors/js/vendors.min.js"></script>
+    <!-- BEGIN Vendor JS-->
+
+    <!-- BEGIN: Page Vendor JS-->
+    <script src="_dist/admin/vendors/js/charts/apexcharts.min.js"></script>
+    <script src="_dist/admin/vendors/js/extensions/tether.min.js"></script>
+    <script src="_dist/admin/vendors/js/extensions/shepherd.min.js"></script>
+    <!-- END: Page Vendor JS-->
+
+    <!-- BEGIN: Theme JS-->
+    <script src="_dist/admin/js/core/app-menu.js"></script>
+    <script src="_dist/admin/js/core/app.js"></script>
+    <script src="_dist/admin/js/scripts/components.js"></script>
+    <!-- END: Theme JS-->
+
+    <!-- BEGIN: Page JS-->
+    <script src="_dist/admin/js/scripts/pages/dashboard-analytics.js"></script>
+    <!-- END: Page JS-->
 </body>
+
 </html>
